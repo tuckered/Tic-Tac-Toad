1189d7a8597b3ca06b209f71707663db15043d3c
tuckered <tom.a.waters@outlook.com>
2018-07-04 14:55:49 +1000

    updated css themes and gamecode

--- app.js
+++ app.js
@@ -10,32 +10,119 @@ var cell7 = document.querySelector('#cell7')
 var cell8 = document.querySelector('#cell8')
 var cell9 = document.querySelector('#cell9')
 
+var player1 = document.querySelector('.player1')
+var player2 = document.querySelector('.player2')
+var winnerDeclare = document.querySelector('.winner')
+
+// winnerDeclare.textContent = "You're the weiner"
 
 var turnCount = 0
 
-var noughtsLineCheck = 0
-
 var gameCounter = function() {
-  turnCount++
+turnCount++
   if (turnCount % 2 === 0) {
-    event.target.classList.add('cell-noughts')
+    event.target.classList.add('cell-crosses')
+    player1.textContent = "Your turn"
     console.log('Player 1\s turn')
 
   } else if (turnCount !== 0) {
-    event.target.classList.add('cell-crosses')
+    event.target.classList.add('cell-noughts')
+    player2.textContent = "Your turn"
     console.log('Player 2\s turn')
   }
-
+  // if statement for conditions of NOUGHTs winning
   if (cell1.classList.contains('cell-noughts') && 
     cell2.classList.contains('cell-noughts') && 
     cell3.classList.contains('cell-noughts')) {
+    winnerDeclare.textContent = "Noughts Wins"
     console.log('noughts win') 
-    } else if (cell1.classList.contains('cell-crosses') && 
+    } else if (cell4.classList.contains('cell-noughts') && 
+    cell5.classList.contains('cell-noughts') && 
+    cell6.classList.contains('cell-noughts')) {
+    winnerDeclare.textContent = "Noughts Wins"
+    console.log('noughts win') 
+    } else if (cell1.classList.contains('cell-noughts') && 
+    cell4.classList.contains('cell-noughts') && 
+    cell7.classList.contains('cell-noughts')) {
+    winnerDeclare.textContent = "Noughts Wins"
+    console.log('noughts win')
+    } 
+    else if (cell2.classList.contains('cell-noughts') && 
+    cell5.classList.contains('cell-noughts') && 
+    cell8.classList.contains('cell-noughts')) {
+    winnerDeclare.textContent = "Noughts Wins"
+    console.log('noughts win')
+    } 
+    else if (cell3.classList.contains('cell-noughts') && 
+    cell6.classList.contains('cell-noughts') && 
+    cell9.classList.contains('cell-noughts')) {
+    winnerDeclare.textContent = "Noughts Wins"
+    console.log('noughts win')
+    } else if (cell1.classList.contains('cell-noughts') && 
+    cell5.classList.contains('cell-noughts') && 
+    cell9.classList.contains('cell-noughts')) {
+    winnerDeclare.textContent = "Noughts Wins"
+    console.log('noughts win')
+    } else if (cell3.classList.contains('cell-noughts') && 
+    cell5.classList.contains('cell-noughts') && 
+    cell7.classList.contains('cell-noughts')) {
+    winnerDeclare.textContent = "Noughts Wins"
+    console.log('noughts win')
+  } 
+  // IF statement for the conditions for CROSSES winning. 
+  if (cell1.classList.contains('cell-crosses') && 
     cell2.classList.contains('cell-crosses') && 
     cell3.classList.contains('cell-crosses')) {
+    winnerDeclare.textContent = "Crosses Wins"
+    console.log('crosses win') 
+    } else if (cell4.classList.contains('cell-crosses') && 
+    cell5.classList.contains('cell-crosses') && 
+    cell6.classList.contains('cell-crosses')) {
+    winnerDeclare.textContent = "Crosses Wins"
+    console.log('crosses win') 
+    } else if (cell1.classList.contains('cell-crosses') && 
+    cell4.classList.contains('cell-crosses') && 
+    cell7.classList.contains('cell-crosses')) {
+    winnerDeclare.textContent = "Crosses Wins"
     console.log('crosses win')
-  }
+    } 
+    else if (cell2.classList.contains('cell-crosses') && 
+    cell5.classList.contains('cell-crosses') && 
+    cell8.classList.contains('cell-crosses')) {
+    winnerDeclare.textContent = "Crosses Wins"
+    console.log('crosses win')
+    } 
+    else if (cell3.classList.contains('cell-crosses') && 
+    cell6.classList.contains('cell-crosses') && 
+    cell9.classList.contains('cell-crosses')) {
+    winnerDeclare.textContent = "Crosses Wins"
+    console.log('crosses win')
+    } else if (cell1.classList.contains('cell-crosses') && 
+    cell5.classList.contains('cell-crosses') && 
+    cell9.classList.contains('cell-crosses')) {
+    winnerDeclare.textContent = "Crosses Wins"
+    console.log('crosses win')
+    } else if (cell3.classList.contains('cell-crosses') && 
+    cell5.classList.contains('cell-crosses') && 
+    cell7.classList.contains('cell-crosses')) {
+    winnerDeclare.textContent = "Crosses Wins"
+    console.log('crosses win')
+  } 
 } 
+
+gameBoard.addEventListener('click', gameCounter)
+
+// Working if statement for top row either noughts or crosses
+// if (cell1.classList.contains('cell-noughts') && 
+//     cell2.classList.contains('cell-noughts') && 
+//     cell3.classList.contains('cell-noughts')) {
+//     console.log('noughts win') 
+//     } else if (cell1.classList.contains('cell-crosses') && 
+//     cell2.classList.contains('cell-crosses') && 
+//     cell3.classList.contains('cell-crosses')) {
+//     console.log('crosses win')
+//   }
+
 
  // WORKING TO IDENTIFY THE CELL CONTENT. NEEDS TO BE WITHIN THE FUNCTION
 // if (cell1.classList.contains('cell-noughts')) {
@@ -57,5 +144,5 @@ var gameCounter = function() {
 
 // gameCell.addEventListener('click', noughtsCell)
 // gameBoard.addEventListener('click', crossesCell)
-gameBoard.addEventListener('click', gameCounter)
+
 
