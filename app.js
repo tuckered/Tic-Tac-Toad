cdce06c9ea84de206030c8294eca36f50e6f4970
tuckered <tom.a.waters@outlook.com>
2018-07-06 11:49:08 +1000

    added readme and uploaded for web hosting

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var cell9 = document.querySelector('#cell9')
 var redToadWins = document.querySelector('#red-toad-wins')
 var blueToadWins = document.querySelector('#blue-toad-wins')
 var drawGame = document.querySelector('#draw-game')
+var toadLegsPic = document.querySelector('.toad-legs')
 
 var player1 = document.querySelector('.player1')
 var player2 = document.querySelector('.player2')
@@ -23,38 +24,36 @@ var resetBtn = document.querySelector('#reset-btn')
 var turnCount = 0
 var currentPlayer = ''
 var winMessage = ''
+var inPlay = true
+
+player2.textContent = 'YOUR TURN!!'
 
 var gameCounter = function() {
-
-  if (!event.target.classList.contains('cell-crosses') && !event.target.classList.contains('cell-noughts')) {
+  gameBoard.classList.remove('toad-legs')
+  if (!event.target.classList.contains('cell-crosses') && !event.target.classList.contains('cell-noughts') && !event.target.classList.contains('gameboard') && inPlay) {
 
     turnCount++
 
     if (turnCount % 2 === 0) {
     event.target.classList.add('cell-crosses')
-    player1.textContent = "Your turn"
-    player2.textContent = ''
+    player2.textContent = "YOUR TURN!!"
+    player1.textContent = ''
     currentPlayer = 'cell-crosses'
-    // winMessage = 'Red Toad Wins!!'
-    // winMessage = redToadWins
     console.log('Player 1\s turn')
 
     } else if (turnCount % 2 === 1) {
       event.target.classList.add('cell-noughts')
-      player2.textContent = "Your turn"
-      player1.textContent = ''
+      player1.textContent = 'YOUR TURN!!'
+      player2.textContent = ''
       currentPlayer = 'cell-noughts'
-      // debugger
-      // winMessage = 'Blue Toad Wins!!'
-      // winMessage = blueToadWins
       console.log('Player 2\s turn')
     }
 
     if (winMessage === '' && turnCount === 9) {
-    // winnerDeclare.textContent = "You guys drew!"
     player2.textContent = ''
     player1.textContent = ''
     drawGame.classList.remove('hide')
+    resetBtn.classList.remove('hide')
     console.log("You guys drew!")
     }
 
@@ -64,15 +63,16 @@ var gameCounter = function() {
       sq3.classList.contains(cellClass)) {
         if (sq1.classList.contains('cell-crosses')) {
           redToadWins.classList.remove('hide')
+          resetBtn.classList.remove('hide')
         } else if (sq1.classList.contains('cell-noughts') && turnCount !== 9) {
           blueToadWins.classList.remove('hide')
+          resetBtn.classList.remove('hide')
+          inPlay = false
         }
-        // winMessage = 'Winner!!'
-      // redToadWins.classList.remove('hide')
-      // winnerDeclare.textContent = winMessage
       player2.textContent = ''
       player1.textContent = ''
-      
+      gameBoard.classList.add('toad-legs')
+
       console.log(winMessage) 
       }
     }
@@ -85,160 +85,315 @@ var gameCounter = function() {
     lineCheck(cell3, cell6, cell9, currentPlayer, winMessage)
     lineCheck(cell1, cell5, cell9, currentPlayer, winMessage)
     lineCheck(cell3, cell5, cell7, currentPlayer, winMessage)
-  }
+  } 
 }
 
 
 var resetGame = function() {
   for (var i = 0; i < allCells.length; i++){
     allCells[i].classList.remove('cell-crosses', 'cell-noughts')
+    redToadWins.classList.add('hide')
+    blueToadWins.classList.add('hide')
+    drawGame.classList.add('hide')
+    resetBtn.classList.add('hide')
     turnCount = 0
     winMessage = ''
-    winnerDeclare.textContent = winMessage
+    inPlay = true
   }
 }
 
-// img.classList.remove('hide')
 
 gameBoard.addEventListener('click', gameCounter)
 resetBtn.addEventListener('click', resetGame)
 
-  // if statement for conditions of NOUGHTs winning
-  //  else if (cell4.classList.contains('cell-noughts') && 
-  //   cell5.classList.contains('cell-noughts') && 
-  //   cell6.classList.contains('cell-noughts')) {
-  //   winnerDeclare.textContent = "Noughts Wins"
-  //   console.log('noughts win') 
-    // } else if (cell1.classList.contains('cell-noughts') && 
-    // cell4.classList.contains('cell-noughts') && 
-    // cell7.classList.contains('cell-noughts')) {
-    // winnerDeclare.textContent = "Noughts Wins"
-    // console.log('noughts win')
-    // } 
-    // else if (cell2.classList.contains('cell-noughts') && 
-    // cell5.classList.contains('cell-noughts') && 
-    // cell8.classList.contains('cell-noughts')) {
-    // winnerDeclare.textContent = "Noughts Wins"
-    // console.log('noughts win')
-    // } 
-    // else if (cell3.classList.contains('cell-noughts') && 
-    // cell6.classList.contains('cell-noughts') && 
-    // cell9.classList.contains('cell-noughts')) {
-    // winnerDeclare.textContent = "Noughts Wins"
-    // console.log('noughts win')
-    // } else if (cell1.classList.contains('cell-noughts') && 
-    // cell5.classList.contains('cell-noughts') && 
-    // cell9.classList.contains('cell-noughts')) {
-    // winnerDeclare.textContent = "Noughts Wins"
-    // console.log('noughts win')
-    // } else if (cell3.classList.contains('cell-noughts') && 
-    // cell5.classList.contains('cell-noughts') && 
-    // cell7.classList.contains('cell-noughts')) {
-    // winnerDeclare.textContent = "Noughts Wins"
-    // console.log('noughts win')
-  // } 
-  // IF statement for the conditions for CROSSES winning. 
-//   var lineCheck = function(sq1, sq2, sq3, cellClass, winMessage) {
-//     if (sq1.classList.contains(cellClass) && 
-//     sq2.classList.contains(cellClass) && 
-//     sq3.classList.contains(cellClass)) {
-//     winnerDeclare.textContent = winMessage
-//     console.log(winMessage) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var gameBoard = document.querySelector('.gameboard')
+
+// var allCells = document.querySelectorAll('.gameboard div')
+// var cell1 = document.querySelector('#cell1')
+// var cell2 = document.querySelector('#cell2')
+// var cell3 = document.querySelector('#cell3')
+// var cell4 = document.querySelector('#cell4')
+// var cell5 = document.querySelector('#cell5')
+// var cell6 = document.querySelector('#cell6')
+// var cell7 = document.querySelector('#cell7')
+// var cell8 = document.querySelector('#cell8')
+// var cell9 = document.querySelector('#cell9')
+
+// var redToadWins = document.querySelector('#red-toad-wins')
+// var blueToadWins = document.querySelector('#blue-toad-wins')
+// var redToadWinHead = document.querySelector('#big-red-toad')
+
+// var drawGame = document.querySelector('#draw-game')
+
+// var player1 = document.querySelector('.player1')
+// var player2 = document.querySelector('.player2')
+// var winnerDeclare = document.querySelector('.winner')
+// var resetBtn = document.querySelector('#reset-btn')
+
+// var turnCount = 0
+// var currentPlayer = ''
+// var winMessage = ''
+// var inPlay = true
+
+// var gameCounter = function() {
+
+//   if (!event.target.classList.contains('cell-crosses') && !event.target.classList.contains('cell-noughts') && !event.target.classList.contains('gameboard') && inPlay) {
+
+//     turnCount++
+
+//     if (turnCount % 2 === 0) {
+//     event.target.classList.add('cell-crosses')
+//     player1.textContent = "Your turn"
+//     player2.textContent = ''
+//     currentPlayer = 'cell-crosses'
+//     console.log('Player 1\s turn')
+
+//     } else if (turnCount % 2 === 1) {
+//       event.target.classList.add('cell-noughts')
+//       player2.textContent = "Your turn"
+//       player1.textContent = ''
+//       currentPlayer = 'cell-noughts'
+//       console.log('Player 2\s turn')
 //     }
+
+//     if (winMessage === '' && turnCount === 9) {
+//     player2.textContent = ''
+//     player1.textContent = ''
+//     drawGame.classList.remove('hide')
+//     resetBtn.classList.remove('hide')
+//     console.log("You guys drew!")
+//     }
+
+//     var lineCheck = function(sq1, sq2, sq3, cellClass, winMessage) {
+//       if (sq1.classList.contains(cellClass) && 
+//       sq2.classList.contains(cellClass) && 
+//       sq3.classList.contains(cellClass)) {
+//         if (sq1.classList.contains('cell-crosses')) {
+//           redToadWins.classList.remove('hide')
+//           resetBtn.classList.remove('hide')
+//         } else if (sq1.classList.contains('cell-noughts') && turnCount !== 9) {
+//           blueToadWins.classList.remove('hide')
+//           resetBtn.classList.remove('hide')
+//         }
+//       player2.textContent = ''
+//       player1.textContent = ''
+//       inPlay = false
+//       console.log(winMessage) 
+//       }
+//     }
+
+//     lineCheck(cell1, cell2, cell3, currentPlayer, message)
+//     lineCheck(cell4, cell5, cell6, currentPlayer, message)
+//     lineCheck(cell7, cell8, cell9, currentPlayer, message)
+//     lineCheck(cell1, cell4, cell7, currentPlayer, message)
+//     lineCheck(cell2, cell5, cell8, currentPlayer, message)
+//     lineCheck(cell3, cell6, cell9, currentPlayer, message)
+//     lineCheck(cell1, cell5, cell9, currentPlayer, message)
+//     lineCheck(cell3, cell5, cell7, currentPlayer, message)
 //   }
+// }
 
 
-//   if (cell1.classList.contains('cell-crosses') && 
-//     cell2.classList.contains('cell-crosses') && 
-//     cell3.classList.contains('cell-crosses')) {
-//     winnerDeclare.textContent = "Crosses Wins"
-//     console.log('crosses win') 
-//     } else if (cell4.classList.contains('cell-crosses') && 
-//     cell5.classList.contains('cell-crosses') && 
-//     cell6.classList.contains('cell-crosses')) {
-//     winnerDeclare.textContent = "Crosses Wins"
-//     console.log('crosses win') 
-//     } else if (cell1.classList.contains('cell-crosses') && 
-//     cell4.classList.contains('cell-crosses') && 
-//     cell7.classList.contains('cell-crosses')) {
-//     winnerDeclare.textContent = "Crosses Wins"
-//     console.log('crosses win')
-//     } 
-//     else if (cell2.classList.contains('cell-crosses') && 
-//     cell5.classList.contains('cell-crosses') && 
-//     cell8.classList.contains('cell-crosses')) {
-//     winnerDeclare.textContent = "Crosses Wins"
-//     console.log('crosses win')
-//     } 
-//     else if (cell3.classList.contains('cell-crosses') && 
-//     cell6.classList.contains('cell-crosses') && 
-//     cell9.classList.contains('cell-crosses')) {
-//     winnerDeclare.textContent = "Crosses Wins"
-//     console.log('crosses win')
-//     } else if (cell1.classList.contains('cell-crosses') && 
-//     cell5.classList.contains('cell-crosses') && 
-//     cell9.classList.contains('cell-crosses')) {
-//     winnerDeclare.textContent = "Crosses Wins"
-//     console.log('crosses win')
-//     } else if (cell3.classList.contains('cell-crosses') && 
-//     cell5.classList.contains('cell-crosses') && 
-//     cell7.classList.contains('cell-crosses')) {
-//     winnerDeclare.textContent = "Crosses Wins"
-//     console.log('crosses win')
-//   } 
-// } 
-
-
-
-// Working if statement for top row either noughts or crosses
-// if (cell1.classList.contains('cell-noughts') && 
-//     cell2.classList.contains('cell-noughts') && 
-//     cell3.classList.contains('cell-noughts')) {
-//     console.log('noughts win') 
-//     } else if (cell1.classList.contains('cell-crosses') && 
-//     cell2.classList.contains('cell-crosses') && 
-//     cell3.classList.contains('cell-crosses')) {
-//     console.log('crosses win')
+// var resetGame = function() {
+//   resetBtn.classList.add('hide')
+//   redToadWins.classList.add('hide')
+//   blueToadWins.classList.add('hide')
+//   for (var i = 0; i < allCells.length; i++){
+//     allCells[i].classList.remove('cell-crosses', 'cell-noughts')
+//     turnCount = 0
+//     // winMessage = ''
+//     // winnerDeclare.textContent = winMessage
 //   }
-
-
- // WORKING TO IDENTIFY THE CELL CONTENT. NEEDS TO BE WITHIN THE FUNCTION
-// if (cell1.classList.contains('cell-noughts')) {
-//   console.log('noughts') 
-//   } else if (cell1.classList.contains('cell-crosses')) {
-//     console.log('crosses')
 // }
 
-
-// var noughtsCell = function(event) {
-//   console.log('hi')
-//   event.target.classList.toggle('cell-noughts')
-// }
-
-// var crossesCell = function(event) {
-//   console.log('hi')
-//   event.target.classList.toggle('cell-crosses')
-// }
-
-// gameCell.addEventListener('click', noughtsCell)
-// gameBoard.addEventListener('click', crossesCell)
+// gameBoard.addEventListener('click', gameCounter)
+// resetBtn.addEventListener('click', resetGame)
 
 
 
 
-// IF STATEMENT BEFORE SEPERATEING OUT STATEMENTS. ONLY BLUE TOAD WINS DISPLAYED
 
-// if (turnCount % 2 === 0) {
-//   event.target.classList.add('cell-crosses')
-//   player1.textContent = "Your turn"
-//   currentPlayer = 'cell-crosses'
-//   winMessage = 'Blue Toad Wins!!'
-//   console.log('Player 1\s turn')
 
-// } else if (turnCount % 2 === 1) {
-//   event.target.classList.add('cell-noughts')
-//   player2.textContent = "Your turn"
-//   currentPlayer = 'cell-noughts'
-//   // debugger
-//   winMessage = 'Red Toad Wins!!'
-//   console.log('Player 2\s turn')
+
+
+
+//   // if statement for conditions of NOUGHTs winning
+//   //  else if (cell4.classList.contains('cell-noughts') && 
+//   //   cell5.classList.contains('cell-noughts') && 
+//   //   cell6.classList.contains('cell-noughts')) {
+//   //   winnerDeclare.textContent = "Noughts Wins"
+//   //   console.log('noughts win') 
+//     // } else if (cell1.classList.contains('cell-noughts') && 
+//     // cell4.classList.contains('cell-noughts') && 
+//     // cell7.classList.contains('cell-noughts')) {
+//     // winnerDeclare.textContent = "Noughts Wins"
+//     // console.log('noughts win')
+//     // } 
+//     // else if (cell2.classList.contains('cell-noughts') && 
+//     // cell5.classList.contains('cell-noughts') && 
+//     // cell8.classList.contains('cell-noughts')) {
+//     // winnerDeclare.textContent = "Noughts Wins"
+//     // console.log('noughts win')
+//     // } 
+//     // else if (cell3.classList.contains('cell-noughts') && 
+//     // cell6.classList.contains('cell-noughts') && 
+//     // cell9.classList.contains('cell-noughts')) {
+//     // winnerDeclare.textContent = "Noughts Wins"
+//     // console.log('noughts win')
+//     // } else if (cell1.classList.contains('cell-noughts') && 
+//     // cell5.classList.contains('cell-noughts') && 
+//     // cell9.classList.contains('cell-noughts')) {
+//     // winnerDeclare.textContent = "Noughts Wins"
+//     // console.log('noughts win')
+//     // } else if (cell3.classList.contains('cell-noughts') && 
+//     // cell5.classList.contains('cell-noughts') && 
+//     // cell7.classList.contains('cell-noughts')) {
+//     // winnerDeclare.textContent = "Noughts Wins"
+//     // console.log('noughts win')
+//   // } 
+//   // IF statement for the conditions for CROSSES winning. 
+// //   var lineCheck = function(sq1, sq2, sq3, cellClass, winMessage) {
+// //     if (sq1.classList.contains(cellClass) && 
+// //     sq2.classList.contains(cellClass) && 
+// //     sq3.classList.contains(cellClass)) {
+// //     winnerDeclare.textContent = winMessage
+// //     console.log(winMessage) 
+// //     }
+// //   }
+
+
+// //   if (cell1.classList.contains('cell-crosses') && 
+// //     cell2.classList.contains('cell-crosses') && 
+// //     cell3.classList.contains('cell-crosses')) {
+// //     winnerDeclare.textContent = "Crosses Wins"
+// //     console.log('crosses win') 
+// //     } else if (cell4.classList.contains('cell-crosses') && 
+// //     cell5.classList.contains('cell-crosses') && 
+// //     cell6.classList.contains('cell-crosses')) {
+// //     winnerDeclare.textContent = "Crosses Wins"
+// //     console.log('crosses win') 
+// //     } else if (cell1.classList.contains('cell-crosses') && 
+// //     cell4.classList.contains('cell-crosses') && 
+// //     cell7.classList.contains('cell-crosses')) {
+// //     winnerDeclare.textContent = "Crosses Wins"
+// //     console.log('crosses win')
+// //     } 
+// //     else if (cell2.classList.contains('cell-crosses') && 
+// //     cell5.classList.contains('cell-crosses') && 
+// //     cell8.classList.contains('cell-crosses')) {
+// //     winnerDeclare.textContent = "Crosses Wins"
+// //     console.log('crosses win')
+// //     } 
+// //     else if (cell3.classList.contains('cell-crosses') && 
+// //     cell6.classList.contains('cell-crosses') && 
+// //     cell9.classList.contains('cell-crosses')) {
+// //     winnerDeclare.textContent = "Crosses Wins"
+// //     console.log('crosses win')
+// //     } else if (cell1.classList.contains('cell-crosses') && 
+// //     cell5.classList.contains('cell-crosses') && 
+// //     cell9.classList.contains('cell-crosses')) {
+// //     winnerDeclare.textContent = "Crosses Wins"
+// //     console.log('crosses win')
+// //     } else if (cell3.classList.contains('cell-crosses') && 
+// //     cell5.classList.contains('cell-crosses') && 
+// //     cell7.classList.contains('cell-crosses')) {
+// //     winnerDeclare.textContent = "Crosses Wins"
+// //     console.log('crosses win')
+// //   } 
+// // } 
+
+
+
+// // Working if statement for top row either noughts or crosses
+// // if (cell1.classList.contains('cell-noughts') && 
+// //     cell2.classList.contains('cell-noughts') && 
+// //     cell3.classList.contains('cell-noughts')) {
+// //     console.log('noughts win') 
+// //     } else if (cell1.classList.contains('cell-crosses') && 
+// //     cell2.classList.contains('cell-crosses') && 
+// //     cell3.classList.contains('cell-crosses')) {
+// //     console.log('crosses win')
+// //   }
+
+
+//  // WORKING TO IDENTIFY THE CELL CONTENT. NEEDS TO BE WITHIN THE FUNCTION
+// // if (cell1.classList.contains('cell-noughts')) {
+// //   console.log('noughts') 
+// //   } else if (cell1.classList.contains('cell-crosses')) {
+// //     console.log('crosses')
+// // }
+
+
+// // var noughtsCell = function(event) {
+// //   console.log('hi')
+// //   event.target.classList.toggle('cell-noughts')
+// // }
+
+// // var crossesCell = function(event) {
+// //   console.log('hi')
+// //   event.target.classList.toggle('cell-crosses')
+// // }
+
+// // gameCell.addEventListener('click', noughtsCell)
+// // gameBoard.addEventListener('click', crossesCell)
+
+
+
+
+// // IF STATEMENT BEFORE SEPERATEING OUT STATEMENTS. ONLY BLUE TOAD WINS DISPLAYED
+
+// // if (turnCount % 2 === 0) {
+// //   event.target.classList.add('cell-crosses')
+// //   player1.textContent = "Your turn"
+// //   currentPlayer = 'cell-crosses'
+// //   winMessage = 'Blue Toad Wins!!'
+// //   console.log('Player 1\s turn')
+
+// // } else if (turnCount % 2 === 1) {
+// //   event.target.classList.add('cell-noughts')
+// //   player2.textContent = "Your turn"
+// //   currentPlayer = 'cell-noughts'
+// //   // debugger
+// //   winMessage = 'Red Toad Wins!!'
+// //   console.log('Player 2\s turn')
---
+++ readme.md
@@ -0,0 +1,30 @@
+# TIC-TAC_TOAD
+
+
+This is a simple browser game based on tic-tac-toe. 2 players take turns to try and get 3 in a row either vetically, hoirzontally or diagonally. 
+
+
+The idea for this game came from a terrible pun. It initially started as bright coloured blocks but once the mechanics were written, I decided to run with the Toad theme for styling. Players use either a blue or red Toad head and there's Super Mario themed font and background. 
+
+
+## THE GAME AND THE RULES
+1. Blue will always start and they get a choice of where to put the first piece. 
+2. Red takes the next turn and turns continue until someone gets a line.
+3. If a winner is decided, the game will stop and no more turns can be taken. A reset button appears if the players want to play again. 
+4. If there is no winner, a DRAW is declared and the reset button will be revealed again. 
+
+
+## PRODUCTION PROCESS
+The game started out as a coloured grid that would get filled when clicked upon. At this point there were no statements to decide how a winner would be decided. 
+
+To take turns, a TURNCOUNT variable was made and added to each time a click was made. An IF statement was used so if TURNCOUNT divisible by 2, it would be player 1's turn and if not it was player 2's turn. 
+
+To determine the win conditions, I decided to use a series of IF statments that would determine the lines and the rows on the play board. Each DIV was given an id number between 1 and 9. There were 8 combinations of winning statements for each player. 
+
+A function lineCheck condensed these win conditions and an IF statement within it specified which player had won. Styling would change depending on the player such as winning text or the reset button appearing. 
+
+## ADDITIONAL FEATURES / PROBLEMS
+1. Currently BLUE TOAD will always start the game. A selector of who will begin the game would be ideal. 
+2. The player indicator does not fit the style of the rest of the game. It just says "YOUR TURN". Changing this to a coloured Toad head or something similar would fit in with the theme. 
+3. There is no counter for how many rounds each player has won. I want to have a feature that the players can pick how many rounds they want to play at the beginning of the game. 
+4. I wanted to have a image pop-up on the gameboard when there is a winner but did not manage to get this implemented without affecting the layout of the existing board. 
