168b78197b296b3bbfb21f2ce732f813b9d2ebb7
Tom <thomasalanwaters@gmail.com>
2018-08-25 17:07:50 +1000

    scaling fixes

--- app.js
+++ app.js
@@ -23,51 +23,53 @@ var resetBtn = document.querySelector('#reset-btn')
 
 var turnCount = 0
 var currentPlayer = ''
-var winMessage = ''
 var inPlay = true
+var winner = false
 
 bluePlayer.textContent = "BLUE'S TURN"
 bluePlayer.classList.add('bluePlayerBorder')
 
 var gameCounter = function() {
   gameBoard.classList.remove('toad-legs')
-  if (!event.target.classList.contains('blue-cell') && !event.target.classList.contains('red-cell') && !event.target.classList.contains('gameboard') && inPlay) {
+  if (!event.target.classList.contains('red-cell') && !event.target.classList.contains('blue-cell') && !event.target.classList.contains('gameboard') && inPlay) {
 
     turnCount++
 
     if (turnCount % 2 === 0) {
-      event.target.classList.add('blue-cell')
+      event.target.classList.add('red-cell')
       bluePlayer.classList.add('bluePlayerBorder')
       redPlayer.classList.remove('redPlayerBorder')
       bluePlayer.textContent = "BLUE'S TURN!!"
       redPlayer.textContent = ''
-      currentPlayer = 'blue-cell'
+      currentPlayer = 'red-cell'
       console.log(bluePlayer.textContent)
     
     } else if (turnCount % 2 === 1) {
-      event.target.classList.add('red-cell')
+      event.target.classList.add('blue-cell')
       redPlayer.classList.add('redPlayerBorder')
       bluePlayer.classList.remove('bluePlayerBorder')
       redPlayer.textContent = "RED'S TURN!!"
       bluePlayer.textContent = ''
-      currentPlayer = 'red-cell'
+      currentPlayer = 'blue-cell'
       console.log(redPlayer.textContent)
     }
 
     
-    var lineCheck = function(sq1, sq2, sq3, cellClass, winMessage) {
+    var lineCheck = function(sq1, sq2, sq3, cellClass) {
       if (sq1.classList.contains(cellClass) && 
       sq2.classList.contains(cellClass) && 
       sq3.classList.contains(cellClass)) {
-        if (sq1.classList.contains('blue-cell')) {
+        if (sq1.classList.contains('red-cell')) {
           redToadWins.classList.remove('hide')
           resetBtn.classList.remove('hide')
           bluePlayer.classList.remove('bluePlayerBorder')
-        } else if (sq1.classList.contains('red-cell') && turnCount !== 9) {
+          winner = true
+        } else if (sq1.classList.contains('blue-cell')) {
           blueToadWins.classList.remove('hide')
           redPlayer.classList.remove('redPlayerBorder')
           resetBtn.classList.remove('hide')
           inPlay = false
+          winner = true
         }
         bluePlayer.textContent = ''
         redPlayer.textContent = ''
@@ -77,16 +79,16 @@ var gameCounter = function() {
       }
     }
     
-    lineCheck(cell1, cell2, cell3, currentPlayer, winMessage)
-    lineCheck(cell4, cell5, cell6, currentPlayer, winMessage)
-    lineCheck(cell7, cell8, cell9, currentPlayer, winMessage)
-    lineCheck(cell1, cell4, cell7, currentPlayer, winMessage)
-    lineCheck(cell2, cell5, cell8, currentPlayer, winMessage)
-    lineCheck(cell3, cell6, cell9, currentPlayer, winMessage)
-    lineCheck(cell1, cell5, cell9, currentPlayer, winMessage)
-    lineCheck(cell3, cell5, cell7, currentPlayer, winMessage)
+    lineCheck(cell1, cell2, cell3, currentPlayer)
+    lineCheck(cell4, cell5, cell6, currentPlayer)
+    lineCheck(cell7, cell8, cell9, currentPlayer)
+    lineCheck(cell1, cell4, cell7, currentPlayer)
+    lineCheck(cell2, cell5, cell8, currentPlayer)
+    lineCheck(cell3, cell6, cell9, currentPlayer)
+    lineCheck(cell1, cell5, cell9, currentPlayer)
+    lineCheck(cell3, cell5, cell7, currentPlayer)
   } 
-  if (winMessage === '' && turnCount === 9) {
+  if (turnCount === 9 && winner === false) {
   bluePlayer.textContent = ''
   redPlayer.textContent = ''
   drawGame.classList.remove('hide')
@@ -98,7 +100,7 @@ var gameCounter = function() {
 
 var resetGame = function() {
   for (var i = 0; i < allCells.length; i++){
-    allCells[i].classList.remove('blue-cell', 'red-cell')
+    allCells[i].classList.remove('red-cell', 'blue-cell')
     redToadWins.classList.add('hide')
     blueToadWins.classList.add('hide')
     drawGame.classList.add('hide')
